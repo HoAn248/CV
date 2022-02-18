bb38b5ec798dee7c4edb4e010f877a42b6236ff5
HoAn248 <hda24082002@gmail.com>
2022-02-18 21:48:10 +0700

    update

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // var audio = new Audio();
-    // audio.src = './audio/HeatWaves.mp3'
-    // audio.loop = true
-    // console.log(audio.src);
-    // audio.play()
-    
-    const audio = document.querySelector('audio')
-    const setVolume = document.querySelector('.volume')
+    let audio = document.querySelector('#audio')
+    let setVolume = document.querySelector('.volume')
     let check = true
-    setTimeout(()=>{
-    audio.play()
-    },4000)
     setVolume.onclick = () => {
         if (check) {
-            setVolume.src = './icon/mute.png'
+            setVolume.src = './icon/sound.png'
             check = false
-            audio.volume = 0
+            audio.play()
         }
         else {
-            setVolume.src = './icon/sound.png'
+            audio.pause()
+            setVolume.src = './icon/mute.png'
             check = true
-            audio.volume = 1
         }
+    }
+    setTimeout(title,1000)
+    function title(){
+        alert('Muốn nghe nhạc, hãy nhấn vào chiếc loa bên phải phía trên màn hình')
     }
 })
